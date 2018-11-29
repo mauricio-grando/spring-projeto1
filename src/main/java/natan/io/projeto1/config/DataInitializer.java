b5456978750b2301837a0712983a4e1c44e68677
Mauricio Grando <mauricio.grando@compasso.com.br>
2018-11-28 22:47:17 -0200

    Crud - insert

---
+++ src/main/java/natan/io/projeto1/config/DataInitializer.java
@@ -0,0 +1,32 @@
+package natan.io.projeto1.config;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import natan.io.projeto1.entity.User;
+import natan.io.projeto1.repository.UserRepository;
+
+@Component
+public class DataInitializer implements ApplicationListener<ContextRefreshedEvent> {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+
+		List<User> users = userRepository.findAll();
+
+		if (users.isEmpty()) {
+			User user = new User();
+			user.setEmail("a@acom");
+			user.setName("Grando");
+			userRepository.save(user);
+		}
+	}
+
+}
